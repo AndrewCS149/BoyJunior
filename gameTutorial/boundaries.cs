f8d054fa29d74fd14948ac608a0b6fe26edff828
Andrew Smith <andrewcs149@gmail.com>
2020-05-25 22:25:50 -0700

    Add boundaries class.

---
+++ gameTutorial/boundaries.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gameTutorial
+{
+
+
+
+    class boundaries
+    {
+        Vector2 guyPosition;
+        GraphicsDeviceManager graphics;
+        Texture2D guySprite;
+        float guySpeed;
+
+        public boundaries()
+        {
+        // set window bounds
+        if (guyPosition.X > graphics.PreferredBackBufferWidth - guySprite.Width / 2)
+            guyPosition.X = graphics.PreferredBackBufferWidth - guySprite.Width / 2;
+        else if (guyPosition.X < guySprite.Width / 2)
+            guyPosition.X = guySprite.Width / 2;
+
+        if (guyPosition.Y > graphics.PreferredBackBufferHeight - guySprite.Height / 2)
+            guyPosition.Y = graphics.PreferredBackBufferHeight - guySprite.Height / 2;
+        else if (guyPosition.Y<guySprite.Height / 2)
+            guyPosition.Y = guySprite.Height / 2;
+        }
+    }
+
+}
